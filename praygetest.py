50499f25ce3134a1742c43c6db060c25a67525c7
Darren Chan <cthdarren88@gmail.com>
2023-04-18 06:01:00 +0800

    holy it was worthwhile to sleep 1 hour

--- praygetest.py
+++ praygetest.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import serial
-import IPython
 from time import sleep
-import random
 import threading
+import cv2
 
 ser = serial.Serial('/dev/serial/by-id/usb-STMicroelectronics_GENERIC_F103C8TX_CDC_in_FS_Mode_6D70229C4955-if00', 9600)
-model = YOLO("yolov8s-pose.pt")
+model = YOLO("yolov8n-pose.pt")
 
 # positions of balls in order (in the snake-like pattern) converted to
 # matrix format in the direction the camera is facing
@@ -51,7 +50,6 @@ def translateCoordToBallPos(coord):
        return ballPos[coord[0]-1][coord[1]-1]
 
 def servoPositions(coordList):
-    print(coordList)
     positions = [200 for x in range(25)]
     if coordList == []:
         return [200 for x in range(25)]
@@ -86,53 +84,83 @@ def servoPositions(coordList):
                 surroundingPos.append(mainPos - 1)
                 
         for pos in [x-1 for x in surroundingPos]:
-            positions[pos] = 100 
+            positions[pos] = 60 
             positions[mainPos - 1] = 0 
         return positions
 
+def mergePositions(combinedList):
+    if len(combinedList) == 0:
+        return [200]*25
+    resultList = combinedList[0]
+    for positionLists in combinedList[1:]:
+        for i, position in enumerate(positionLists):
+            if position < 199 and position < resultList[i]:
+                resultList[i] = position
+
+    return resultList
+
 thr1 = threading.Thread(target=createColorWave)
 thr1.start()
+cap = cv2.VideoCapture(0)
 
-results = model.predict(source="0", show=True, stream=True)
-for result in results:
-    kp = result.keypoints
-    print(kp)
-    coordList = []
-    try:
-        for person in kp:
-            left_foot = person[-1][0], person[-1][1]
-            right_foot = person[-2][0], person[-2][1]
-            coords = (left_foot[0].item() + right_foot[0].item()) /2 ,( left_foot[1].item() + right_foot[1].item() )/ 2
-            if coords[0] > 1000 or coords[0] < 1 or coords[1] > 1000 or coords[1] < 1:
-                continue
-            print(coords)
-            if 428.6 < coords[1] or coords[1] < 245.6:
-                continue
-            elif coords[1] < 254.6:
-                row = 1
-                col = checkCol(coords[0], 156, 210.6, 281, 339, 416.6, 478)
-            elif coords[1] < 277.6:
-                row = 2
-                col = checkCol(coords[0], 140, 206, 275, 346, 424, 500)
-            elif coords [1] < 303.6:
-                row = 3
-                col = checkCol(coords[0], 109, 190, 275, 360, 450, 526)
-            elif coords[1] < 350.6:
-                row = 4
-                col = checkCol(coords[0], 72, 173, 272, 369, 464, 561)
-            else:
-                row = 5
-                col = checkCol(coords[0], 26, 134, 257, 388, 520, 616)
-            if col == None:
-                continue
-            coordList.append([row,col])
-        print(coordList)
-            
-        servoPos = servoPositions(coordList)
-    except IndexError:
-        print("No legs detected") 
-        continue
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
 
+    if success:
+        # Run YOLOv8 inference on the frame
+        bigList = []
+        results = model(frame, line_thickness = 1)
+        for result in results:
+            kp = result.keypoints
+            try:
+                for person in kp:
+                    coordList = []
+                    left_foot = person[-1][0], person[-1][1]
+                    right_foot = person[-2][0], person[-2][1]
+                    coords = (left_foot[0].item() + right_foot[0].item()) /2 ,( left_foot[1].item() + right_foot[1].item() )/ 2
+
+                    if 428.6 < coords[1] or coords[1] < 245.6:
+                        continue
+                    elif coords[1] < 254.6:
+                        row = 1
+                        col = checkCol(coords[0], 156, 210.6, 281, 339, 416.6, 478)
+                    elif coords[1] < 277.6:
+                        row = 2
+                        col = checkCol(coords[0], 140, 206, 275, 346, 424, 500)
+                    elif coords [1] < 303.6:
+                        row = 3
+                        col = checkCol(coords[0], 109, 190, 275, 360, 450, 526)
+                    elif coords[1] < 350.6:
+                        row = 4
+                        col = checkCol(coords[0], 72, 173, 272, 369, 464, 561)
+                    else:
+                        row = 5
+                        col = checkCol(coords[0], 26, 134, 257, 388, 520, 616)
+                    if col == None:
+                        continue
+                    coordList.append([row,col])
+                    bigList.append(servoPositions(coordList))
+
+            except IndexError:
+                print("No legs detected") 
+                continue
+
+        servoPos = mergePositions(bigList)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
 # Use PySerial to talk to serial ports.
 # On *nix, this will be /dev/<something>
 
